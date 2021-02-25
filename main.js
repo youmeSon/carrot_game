27f8fe720ea986509270ddb5e4e6f9d0735575d1
Youme <winellyoume@gmail.com>
2021-02-25 14:18:14 +0000

    JS: Add timer countdown

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const timer = document.querySelector('.timer');
 const result = document.querySelector('.result');
 const popUpResult = document.querySelector('.popup');
 
+//timer counter
+
+let counterInterval = null;
+let counter = 10;
+let counterRunning = false;
+
 // sounds 
 const bgAudio = new Audio('sound/bg.mp3');
 const failAudio = new Audio('sound/alert.wav');
@@ -12,10 +18,14 @@ const bugPullAudio = new Audio('sound/bug_pull.mp3');
 const carrotPullAudio = new Audio('sound/carrot_pull.mp3');
 const winAudio = new Audio('sound/game_win.mp3');
 
-//Option. fail
-function onFail() {
+function gameOver(messageText){
   bgAudio.pause();
   failAudio.play();
+  playBtn.classList.add('active');
+  popUpResult.classList.add('active');
+  result.textContent = messageText;
+  counterRunning = false;
+  clearInterval(counterInterval);
 }
 
 
@@ -24,60 +34,60 @@ function onPlay() {
   bgAudio.play(); 
  // change to the pause button
   playBtn.innerHTML = `<i class="fas fa-stop" data-id="stopMusic"></i>`;
-  //2. timer start 
+  //2. timer start
+  counterRunning = true;
   onTimer();
   //3. bugs & carrot appear
   //4. count start
 }
-function onStop() {
-  bgAudio.pause();
-}
 
 // timer function 
-let timeLeft = 10;
 function onTimer() {
-  const countdownTimer = setInterval(function() {
-    if(timeLeft <= 0) {
-      clearInterval(countdownTimer);
-      popUpResult.classList.add('active');
-      result.textContent = `You Failed!❌`;
-      onFail();
-    }
-    timer.textContent = `0:${timeLeft}`;
-    timeLeft -= 1;
+  counterInterval = setInterval(function() {
+    incrementCounter();
   }, 1000);  
+};
+
+function incrementCounter(){
+  if(counter > 0 && counterRunning){
+    counter--;
+    timer.textContent = `0:${(counter > 9 ? counter : "0" + counter)}`;
+  } else if(counter == 0){
+    clearInterval(counterInterval);
+    gameOver("You Failed!❌");
+  }
 };
 
 
 // add bugs and carrots 
-const gameField = document.querySelector('.gameField');
-let carrotNum = 0;
-function createCarrot() {
-  const newCarrot = document.createElement('');
-  newCarrot.innerHTML = `<img id="carrot" src="img/carrot.png" alt="carrot" data-carrotNum="${carrotNum}">`;
-  carrotNum++;
-  gameField.appendChild(newCarrot);
-}
+// const gameField = document.querySelector('.gameField');
+// let carrotNum = 0;
+// function createCarrot() {
+//   const newCarrot = document.createElement('');
+//   newCarrot.innerHTML = `<img id="carrot" src="img/carrot.png" alt="carrot" data-carrotNum="${carrotNum}">`;
+//   carrotNum++;
+//   gameField.appendChild(newCarrot);
+// }
 
-let bugNum = 0;
-function createBug() {
-  const newBug = document.createElement('img');
-  newBug.innerHTML = `<img id="bug" src="img/bug.png" alt="bug" data-num="${bugNum}">`;
-  bugNum++;
-  gameField.appendChild(newBug);
-}
+// let bugNum = 0;
+// function createBug() {
+//   const newBug = document.createElement('img');
+//   newBug.innerHTML = `<img id="bug" src="img/bug.png" alt="bug" data-num="${bugNum}">`;
+//   bugNum++;
+//   gameField.appendChild(newBug);
+// }
   
 
 // Event Listener 
 playBtn.addEventListener('click', (e) => {
   if(e.target.dataset.id === "stopMusic") {
     playBtn.innerHTML = `<i class="fas fa-play"></i>`;
-    onStop();
+    gameOver("Replay❓"); 
   } else{
     onPlay();
   }
 
-  createBug();
-  createCarrot();
+  // createBug();
+  // createCarrot();
 });
 
